f1927d0250d8bcec0e44a714afb00f995f938e5c
nakamuraHAR <zoffiasyu@hotmail.co.jp>
2016-07-24 13:13:30 +0900

    検索機能まで実装

--- PCInfoEditor/PCInfoEditor/Models/PCInfo.cs
+++ PCInfoEditor/PCInfoEditor/Models/PCInfo.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace PCInfoEditor.Models
 {
@@ -33,47 +34,50 @@ namespace PCInfoEditor.Models
         /// <summary>
         /// PC管理番号
         /// </summary>
+        [Display(Name="PC管理番号")]
         public string SerialNumber { get; set; }
 
         /// <summary>
         /// IPアドレス
         /// </summary>
+        [Display(Name = "IPアドレス")]
         public string IPAddress { get; set; }
 
         /// <summary>
         /// 使用区分（サーバ/個人/共有）
         /// </summary>
+        [Display(Name = "使用区分")]
         public UseSegment UseSegment { get; set; }
 
         /// <summary>
         /// 機種
         /// </summary>
+        [Display(Name = "機種")]
         public string MachineType { get; set; }
 
         /// <summary>
         /// 型番
         /// </summary>
+        [Display(Name = "型番")]
         public string ModelNumber { get; set; }
 
         /// <summary>
         /// 使用者名
         /// </summary>
+        [Display(Name = "使用者名")]
         public string UserName { get; set; }
 
         /// <summary>
         /// PC名
         /// </summary>
+        [Display(Name = "PC名")]
         public string PCName { get; set; }
 
         /// <summary>
-        /// 備考１
+        /// 備考
         /// </summary>
-        public string Remark1 { get; set; }
-
-        /// <summary>
-        /// 備考２
-        /// </summary>
-        public string Remark2 { get; set; }
+        [Display(Name = "備考")]
+        public string Remark { get; set; }
     }
 
     public class PCInfoDBContext : DbContext
